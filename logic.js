f38a53f0b798f8f691de5acee85394b84e39b5cf
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-10 20:21:39 -0700

    made it so that instead of pulling from three different arrays, the function combines necessary arrays into one to streamline it. Which means the 100 lines of code I wrote above are garbage

--- logic.js
+++ logic.js
@@ -99,17 +99,48 @@
 
 var button = document.querySelector('button');
 var textArea = document.querySelector('#password-area');
-var lettersArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-                        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
-                        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//var lettersArray;
 var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharactersArray = ['&amp;', '&excl;', '&num;', 
     '&dollar;', '&percnt;', '&quest;', '&commat;', 
     '&plus;', '&lt;', '&gt;'];
-var passwordArray;
+var passwordArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 var password = '';
 
 
-function passwordGenerator(special, nums){
+
+
+function buttonPress (){
+    var upperCase = confirm('Does your password need upper case letters?') 
+    var specialCharacters = confirm('Does your password need special characters?');
+    var numbers = confirm('Does your password need numbers?');
+    var passwordLength = parseInt(prompt('How long would you like your password? (type any number between 8-128'));
     
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+        alert('Invalid selection: Please try again, making sure your length is a number between 8-128.');
+    } else {
+
+    } 
+
+                
 };
+
+passwordGenerator(true, true);;
+
+
+function passwordGenerator (special, numbers){
+
+    if (special === true){
+        for (var i = 0; i< specialCharactersArray.length; i++){
+           passwordArray.push(specialCharactersArray[i]); 
+        }
+    }
+    if (numbers === true){
+        for (var i = 0; i< specialCharactersArray.length; i++){
+           passwordArray.push(numbersArray[i]); 
+        }
+    }
+console.log(passwordArray);
+}
